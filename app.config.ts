af71144701c3149d1b58a135ff1779ab4a6c90a8
CHENG-HUNG, LU <sumily20@gmail.com>
2023-09-02 18:54:27 +0000

    - logo change - add some dir/md
    Co-authored-by: Kevin Chang <kevin0216@users.noreply.github.com>

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,11 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: '四零四網路資訊|官方說明',
+    description: '這裡應該能解決您的大多數問題。',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      // twitter: 'nuxtstudio',
+      github: 'm4nic-tw'
     },
     aside: {
       level: 0,
@@ -15,10 +15,16 @@ export default defineAppConfig({
       logo: true
     },
     footer: {
-      iconLinks: [
+      // iconLinks: [
+      //   {
+      //     href: 'https://nuxt.com',
+      //     icon: 'simple-icons:nuxtdotjs'
+      //   }
+      // ],
+      textLinks: [
         {
-          href: 'https://nuxt.com',
-          icon: 'simple-icons:nuxtdotjs'
+          text: '營業人名稱: 四零四網路資訊企業社 | 統一編號: 92527380',
+          href: ''
         }
       ]
     }
